025f9fe51fad3a3db43644ee91b0a65ffbe50396
gomar0801 <margot.utrera@edu.esiee.fr>
2025-07-21 15:44:35 +0200

    Update display_and_play_partition.py

--- display_and_play_partition.py
+++ display_and_play_partition.py
@@ -1,18 +1,18 @@
 from music21 import converter
 
-def display_and_play(xml_path, show_score=True, midi_path=None):
+def display_and_play(xml_path, show_score=True, midi=True):
     """
     Affiche une partition et génère un fichier MIDI à partir d’un fichier MusicXML.
 
     Args:
         xml_path (str): chemin du fichier MusicXML (.musicxml)
         show_score (bool): True pour afficher la partition dans une fenêtre
-        midi_path (str): chemin du fichier .midi à générer (facultatif)
+        midi (bool): True pour ecouter le fichier midi 
     """
     score = converter.parse(xml_path)
     
     if show_score:
         score.show()  # Affiche la partition (peut ouvrir MuseScore ou une fenêtre PDF)
 
-    if midi_path:
-        score.write('midi', fp=midi_path)
+    if midi:
+        score.show('midi')
