fd2cc6d96f7e974066331d2090e72cedeae9f0f3
gomar0801 <margot.utrera@edu.esiee.fr>
2025-07-21 11:32:34 +0200

    Create tokens_to_musicxml.py

---
+++ tokens_to_musicxml.py
@@ -0,0 +1,16 @@
+from tokens_to_score import tokens_to_score
+
+def convert_tokens_to_musicxml(tokens, output_path):
+    """
+    Convertit une séquence de tokens en un fichier MusicXML (.musicxml).
+    Args:
+        tokens (List[str] or str): liste ou chaîne de tokens (doit contenir 'R' et 'L')
+        output_path (str): chemin du fichier .musicxml de sortie
+    """
+    if isinstance(tokens, list):
+        token_string = " ".join(tokens)
+    else:
+        token_string = tokens
+
+    score = tokens_to_score(token_string)
+    score.write('musicxml', fp=output_path)
