6aa8e7163037c44ca29975320d02e0e755156354
racousin <racousin@student.42.fr>
2025-07-29 14:11:18 +0200

    add tests

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,11 @@
 # Install MuseScore
 brew install --cask musescore 2>/dev/null || apt-get install -y musescore
 
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+pip install -r requirements.txt
+pip install .
+else
 # Install the requirements
 pip install -r BachGen/requirements.txt
 pip install ./BachGen
+fi
