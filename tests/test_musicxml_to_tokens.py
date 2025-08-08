b86d45ce1864c8410c81806f674b910c41316843
gomar0801 <margot.utrera@edu.esiee.fr>
2025-08-08 15:47:18 +0200

    Update test_musicxml_to_tokens.py

--- tests/test_musicxml_to_tokens.py
+++ tests/test_musicxml_to_tokens.py
@@ -4,7 +4,7 @@ from bachgen.score_to_tokens_solution_all2 import MusicXML_to_tokens
 def test_minimal_xml_to_tokens():
     """Test converting minimal MusicXML to tokens"""
     tokens = MusicXML_to_tokens('musicxml_sample/minimal.musicxml')
-    expected = ['R', 'bar', 'clef_treble', 'key_natural_0', 'time_4/4', 'note_C4', 'len_4', 'L', 'bar']
+    expected = ['R', 'bar', 'clef_treble', 'key_natural_0', 'time_4/4', 'note_C4', 'len_4', 'L', 'bar', 'key_natural_0', 'time_4/4', 'clef_bass']
     assert tokens == expected
 '''
 def test_xml_to_tokens_with_note_numbers():
@@ -104,3 +104,4 @@ def test_chevauchement_xml_to_tokens():
         'L', 'bar', 'key_natural_0', 'time_4/4', 'clef_bass'
     ]
     assert tokens == expected
+
